699463291f9992b37311035d850670aa74331d72
TristanBqn <144691572+TristanBqn@users.noreply.github.com>
2026-08-21 17:41:35 +0200

    feat: add indexed Finder window command

--- Sources/WindowsMacCore/WindowsMacCommand.swift
+++ Sources/WindowsMacCore/WindowsMacCommand.swift
@@ -5,13 +5,20 @@ public struct WindowsMacCommand: Codable, Equatable, Sendable {
         case snap
         case finderOpenChild
         case finderAddressBar
+        case finderActivateWindow
     }
 
     public let command: Action
     public let direction: Direction?
+    public let index: Int?
 
-    public init(command: Action, direction: Direction? = nil) {
+    public init(
+        command: Action,
+        direction: Direction? = nil,
+        index: Int? = nil
+    ) {
         self.command = command
         self.direction = direction
+        self.index = index
     }
 }
